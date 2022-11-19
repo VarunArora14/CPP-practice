bb2c70f1cda1b4362c5f37ccad04511f58bce900
RDXV <varunarora1408@gmail.com>
2022-11-19 11:52:41 +0530

    new practice file added

--- practice/try.cpp
+++ practice/try.cpp
@@ -9,7 +9,12 @@
 
 #define print1(x) cout << #x << '=' << x << endl
 #define print2(x, y) cout << #x << '=' << x << ',' << #y << '=' << y << endl
-#define nline cout << endl
+#define nline cout<<endl
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;i<n;i++)
+#define pb push_back
+#define F first
+#define S second
 
 using namespace std;
 
@@ -17,12 +22,14 @@ using vi = vector<int>;
 using vvi = vector<vector<int>>;
 using ll = long long;
 using l = long;
+using vll = vector<long long>;
 using pii = pair<int, int>;
+using pll = pair<long, long>;
 using vpii = vector<pair<int, int>>;
 
+const int mod = 1000000007;
 int main()
 {
-    int n = 2;
-    cout << (n & 1); 
-    return 0;
-}
+
+return 0;
+}   
